9dcd70b27d344faade045856f035c7eb4fa79ad1
lsw71311 <lsw71311@naver.com>
2024-03-28 17:11:33 +0900

    feat: jwt토큰 추출 및 암호화 설정

--- src/main/java/com/example/myservice/global/jwt/JwtUtil.java
+++ src/main/java/com/example/myservice/global/jwt/JwtUtil.java
@@ -1,4 +1,4 @@
-package com.example.myservice.auth.jwt;
+package com.example.myservice.global.jwt;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
@@ -6,9 +6,6 @@ import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
